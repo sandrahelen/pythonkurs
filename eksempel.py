67e1abd4c736abd1bcb84f6ab2cfdd9ee3f02ed5
sandrahelen <sandrahh@stud.ntnu.no>
2020-04-20 11:58:42 +0000

    UPDATE

---
+++ eksempel.py
@@ -0,0 +1,35 @@
+'''1. Print'''
+print("Hei")        #tekst
+print(1)            #tall
+print("Hei", 1)     #tekst og tall
+print("-" * 20)
+
+'''2. Summering av tall'''
+print("1 + 1 =", 1 + 1)
+print("-" * 20)
+
+'''3. Variabler'''
+#I programmering kan en variabel være et tall, men også en tekststreng
+tall = 2
+tekst = "tekst"
+print(tall, tekst) #skriver ut variabler
+
+#Legge sammen variabler
+x = 2
+y = "en"
+print("tall + x =", tall + x)
+print("tekst + y =", tekst + y)
+
+#Ny variabel
+nyVariabel = tall + x
+print("Ny variabel =", nyVariabel)
+print("-" * 20)
+
+'''4. Regnerekkefølge'''
+#Potenser -> Divisjon -> Multiplikasjon -> Addisjon og subtraksjon
+print("2^2 =", 2**2) #potenser
+print("2 * 2 =", 2 * 2) #multiplikasjon
+print("4 / 2 =", 4 / 2) #divisjon
+print("5 // 3 =", 5 // 2) #heltallsdivision
+print("")
+
